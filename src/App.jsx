0cc1ae59f4d0d5b0e208434989ebc8924d1e4ae7
Frenzoid <frenzoid@protonmail.com>
2025-06-07 14:15:00 +0200

    patched burger

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,66 @@
+import { useEffect, useRef } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
 import HomePage from './HomePage';
 import GalleryPage from './GalleryPage';
 import EventsPage from './EventsPage';
+
 import logo from './assets/logo.png';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 import './styles.css';
 
 function App() {
+  // a ref to the collapse container
+  const collapseRef = useRef(null);
+
+  // helper to close the menu
+  const closeNavbar = () => {
+    const collapseEl = collapseRef.current;
+    if (collapseEl.classList.contains('show')) {
+      // Option A: directly remove the Bootstrap class
+      collapseEl.classList.remove('show');
+    }
+  };
+
+  // add click listeners to all nav-links once
+  useEffect(() => {
+    const links = document.querySelectorAll('.nav-link');
+    links.forEach(link => {
+      link.addEventListener('click', closeNavbar);
+    });
+    // cleanup
+    return () => {
+      links.forEach(link => {
+        link.removeEventListener('click', closeNavbar);
+      });
+    };
+  }, []);
+
   return (
     <Router basename={import.meta.env.BASE_URL}>
       <div className="app">
         <nav className="navbar navbar-expand-lg navbar-dark">
           <div className="container">
-            <Link className="navbar-brand" to="/">
+            <Link className="navbar-brand" to="/" onClick={closeNavbar}>
               <img src={logo} alt="Grotto Laura by Casa Ana" className="logo-img" />
               Grotto Laura by<br /> Casa Ana
             </Link>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+            >
               <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
+            <div
+              className="collapse navbar-collapse"
+              id="navbarNav"
+              ref={collapseRef}
+            >
               <ul className="navbar-nav ms-auto">
                 <li className="nav-item">
                   <Link className="nav-link" to="/">Home</Link>
@@ -63,9 +103,9 @@ function App() {
               </div>
             </div>
           </div>
-        </footer >
-      </div >
-    </Router >
+        </footer>
+      </div>
+    </Router>
   );
 }
 
